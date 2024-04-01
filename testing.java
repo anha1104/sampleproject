d8726358e06aef54bd8d757143b543263fe3197d
anha1104 <aha25@uw.edu>
2024-03-31 22:24:16 -0700

    1st commit from An

--- testing.java
+++ testing.java
@@ -1,6 +1,7 @@
 public class testing {
     public static void main(String[] args) {
-        System.out.println("hello");
+        System.out.println("rdzkvgxgbxf");
+        System.out.println("fvdjhbvudbhbvbdbvjdbvhdf");
     }
 
 }
